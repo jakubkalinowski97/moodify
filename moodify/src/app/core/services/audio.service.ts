8aa3d185012fc65723b2f4fe6bc4c27bceca65c7
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2023-03-06 21:42:47 +0100

    added missed package

--- moodify/src/app/core/services/audio.service.ts
+++ moodify/src/app/core/services/audio.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { formatDuration } from 'date-fns';
-import { BehaviorSubject, Observable, retry, Subject, takeUntil, tap } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, takeUntil } from 'rxjs';
 import { AudioEvents } from 'src/app/models/audio-events.enum';
 import { StreamState } from 'src/app/models/stream-state';
 
