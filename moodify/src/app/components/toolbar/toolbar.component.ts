85017b3154b72ad91448dc29fd5cdb772e808e9f
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2023-03-09 00:43:36 +0100

    Displayed user name in the toolbar

--- moodify/src/app/components/toolbar/toolbar.component.ts
+++ moodify/src/app/components/toolbar/toolbar.component.ts
@@ -1,22 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AudioService } from 'src/app/core/services/audio.service';
 import { ToolbarService } from './toolbar.service';
 import packageJson from '../../../../package.json';
+import { AuthService } from '../../core/services/auth.service';
+import { Observable } from 'rxjs';
+import { User } from 'netlify-identity-widget';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnInit {
   appVersion = packageJson.version;
-  constructor(private toolbarService: ToolbarService, private audioService: AudioService) {}
+  user$!: Observable<User | null>;
+  constructor(private toolbarService: ToolbarService, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getUser();
+  }
 
   toggle(): void {
     this.toolbarService.toggle();
-  }
-
-  setVolume(value: number): void {
-    this.audioService.setVolume(value);
   }
 }
