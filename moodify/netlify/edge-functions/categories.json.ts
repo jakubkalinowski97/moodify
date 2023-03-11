4b9ae20ffda0f9508dba5cd9f42272cee9abeae0
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2023-03-11 20:43:14 +0100

    edge functions integrated

--- moodify/netlify/edge-functions/categories.json.ts
+++ moodify/netlify/edge-functions/categories.json.ts
@@ -1,20 +1,17 @@
-import type { Context } from 'https://edge.netlify.com'
-import { connect } from 'https://unpkg.com/@planetscale/database@^1.1'
+import { Handler } from "@netlify/functions";
+import { withPlanetscale } from "@netlify/planetscale";
 
-export default async function handler(req: Request, context: Context): Promise<Response> {
-    const connection = connect({
-        host: Deno.env.get('PLANETSCAPE_DATABASE_HOST'),
-        username: Deno.env.get('PLANETSCAPE_DATABASE_USERNAME'),
-        password: Deno.env.get('PLANETSCAPE_DATABASE_PASSWORD'),
-    })
+export const handler: Handler = withPlanetscale(async (_event, context) => {
+  const {
+    planetscale: { connection },
+  } = context;
 
-    const categories = await connection.execute('SELECT * FROM category;', []);
+  const categories = await connection.execute("SELECT * FROM category");
 
-    const json = JSON.stringify(categories);
+  const data = JSON.stringify(categories);
 
-    return new Response(json, {
-        headers: {
-          'content-type': 'application/json;charset=UTF-8',
-        }
-      });
-}
+  return {
+    body: data,
+    statusCode: 200,
+  };
+});
