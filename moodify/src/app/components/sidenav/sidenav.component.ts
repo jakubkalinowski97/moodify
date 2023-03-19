291b5371b290a0141e676eee3162384b08929fa6
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2023-03-19 14:38:21 +0100

    Ajdusted sidenav rwd

--- moodify/src/app/components/sidenav/sidenav.component.ts
+++ moodify/src/app/components/sidenav/sidenav.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToolbarService } from '../toolbar/toolbar.service';
 
 @Component({
   selector: 'app-sidenav',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class SidenavComponent {
 
+  constructor(private toolbarService: ToolbarService) {}
+
+  closeSidenav(): void {
+    this.toolbarService.close();
+  }
 }
