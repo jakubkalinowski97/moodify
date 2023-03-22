99f3bc92f674f2e1e53133621320e3dffad8ba0d
Jakub Kalinowski <jakubkalinowski97@gmail.com>
2023-03-22 22:02:13 +0100

    Init admin module

---
+++ moodify/src/app/pages/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FileUploaderComponent } from './nested-components/file-uploader/file-uploader.component';
+
+
+
+@NgModule({
+  declarations: [
+    FileUploaderComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
